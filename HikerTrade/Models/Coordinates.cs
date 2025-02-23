0b47e768ad22201c34e10b29148303c4ee75c7dd
Emilia Repo <emiliarepo@icloud.com>
2025-02-23 20:59:00 +0200

    Change Coordinates to record

--- HikerTrade/Models/Coordinates.cs
+++ HikerTrade/Models/Coordinates.cs
@@ -1,10 +1,7 @@
 namespace HikerTrade.Models;
 
-public class Coordinates(double longitude, double latitude)
+public record Coordinates(double Longitude, double Latitude)
 {
-    public double Longitude { get; } = longitude;
-    public double Latitude { get; } = latitude;
-
     public override string ToString()
     {
         return $"({Longitude}, {Latitude})";
